484c9b2f60eb9fcee086f0355ef461c4bf98919d
philadamson93 <philadamson93@gmail.com>
2023-12-10 13:52:50 -0800

    add jupyter notebook for analyzing results

--- dfd_mr_recon/reader_study_metrics.py
+++ dfd_mr_recon/reader_study_metrics.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import numpy as np
 from PIL import Image
 import pandas as pd
@@ -15,7 +16,7 @@ from meddlr.metrics.image import SSIM, PSNR
 
 def load_image(image_path):
     # Load an image and convert it to a tensor
-    image = Image.open(image_path).convert('L')  # Convert to grayscale if necessary
+    image = Image.open(image_path).convert('L') # Convert to grayscale
     transform = transforms.ToTensor()
     image_tensor = transform(image)
     image_tensor = image_tensor.unsqueeze(0) # Shape: 1x1xHxW
@@ -25,7 +26,7 @@ def load_image(image_path):
 def compute_metrics(img_dir, results_dir, metrics_dict):
 
     methods = ["UNet", "Unrolled"]
-    accelerations = ["2x", "4x", "6x"]
+    accelerations = [2.0, 4.0, 6.0]
    
     results = []
 
@@ -35,7 +36,7 @@ def compute_metrics(img_dir, results_dir, metrics_dict):
             recon_folder = os.path.join(img_dir, folder_name)
 
             for recon_file in sorted(os.listdir(recon_folder)):
-                patient_name = recon_file.split('_')[-1].split('.')[0]
+                scanID = recon_file.split('_')[-1].split('.')[0]
                 recon_image_path = os.path.join(recon_folder, recon_file)
                 gt_image_path = os.path.join(img_dir, 'gt', f"gt_{patient_name}.png")
 
@@ -44,12 +45,11 @@ def compute_metrics(img_dir, results_dir, metrics_dict):
 
                 img_result = {
                             "Acceleration": acc,
-                            "Reconstruction Method": method,
-                            "Patient Name": patient_name,
+                            "Method": method,
+                            "ScanID": scanID,
                 }
 
                 for metric_name, metric in metrics_dict.items():
-                    # print(metric_name, metric)
                     img_result[metric_name] = metric(recon_image, gt_image).item()
                 
                 results.append(img_result)
@@ -61,8 +61,15 @@ def compute_metrics(img_dir, results_dir, metrics_dict):
 
 if __name__ == "__main__":
 
-    img_dir = "/bmrNAS/people/padamson/results/Perceptual_Loss/Reader_Study_Data/Reader_Study_png"
-    results_dir = "/bmrNAS/people/padamson/results/meddlr/ssfd/reader_study_script"
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--img_dir", default="./image_dir", help="Specify the top level folder of your MR recon image directory. (default: './image_dir')")
+    parser.add_argument("--results_dir", default="./results", help="Specify the folder to save the results csv file. (default: './results')")
+    
+    args = parser.parse_args()
+
+    img_dir = args.img_dir
+    results_dir = args.results_dir
 
     metrics_dict = {"SSIM": SSIM(im_type = None),
                     "PSNR": PSNR(im_type = None),
